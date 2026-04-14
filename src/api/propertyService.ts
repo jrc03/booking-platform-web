c5f46829f12c38a8fa0c99c1191615b4f6a845df
jrc03 <joserdlc2727@gmail.com>
2026-04-13 21:00:49 -0400

    feat: setup property API service

---
+++ src/api/propertyService.ts
@@ -0,0 +1,28 @@
+import type { PropertyResponseDto } from "../types/dtos";
+import api from "./axios";
+
+export interface SearchPropertyQuery {
+  location?: string;
+  startDate?: string;
+  endDate?: string;
+  minCapacity?: number;
+  maxPrice?: number;
+  pageNumber?: number;
+  pageSize?: number;
+}
+
+export const propertyService = {
+
+  search: async (query?: SearchPropertyQuery): Promise<PropertyResponseDto[]> => {
+    const response = await api.get<PropertyResponseDto[]>("/properties/search", {
+      params: query || {}
+    });
+    return response.data;
+  },
+
+  // GET: /api/properties/{id}
+  getById: async (id: string): Promise<PropertyResponseDto> => {
+    const response = await api.get<PropertyResponseDto>(`/properties/${id}`);
+    return response.data;
+  }
+};
